564e8126792c5bc2ceccc899cd9e02f695b6192f
mmt9497 <krishan.varshney@go-mmt.com>
2023-07-02 00:53:25 +0530

    Fix: 'JSONViewer' initializer is inaccessible due to 'internal' protection level

--- Sources/JSONViewer/JSONViewer.swift
+++ Sources/JSONViewer/JSONViewer.swift
@@ -3,6 +3,10 @@ import SwiftUI
 public struct JSONViewer: View {
     var rootNode: JSONNode
     
+    public init(rootNode: JSONNode) {
+        self.rootNode = rootNode
+    }
+    
     public var body: some View {
         HStack {
             VStack {
